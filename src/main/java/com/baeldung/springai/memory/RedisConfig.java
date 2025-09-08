551c2a1f058eb15c5c4c6b69d13bcbb5b0849428
okruti-tjain <tjain@okruti.com>
2025-09-08 14:26:59 +0530

    start the Application with Redis.

--- src/main/java/com/baeldung/springai/memory/RedisConfig.java
+++ src/main/java/com/baeldung/springai/memory/RedisConfig.java
@@ -1,10 +1,11 @@
 package com.baeldung.springai.memory;
 
+
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import redis.clients.jedis.JedisPooled;
 
 
@@ -18,7 +19,10 @@ public class RedisConfig {
 
     @Bean
     public VectorStore redisVectorStore(JedisPooled jedisPooled, EmbeddingModel embeddingModel) {
-        return RedisVectorStore.builder(jedisPooled, embeddingModel).build();
+        return RedisVectorStore.builder(jedisPooled, embeddingModel)
+                .indexName("spring-ai-index")
+                .vectorAlgorithm(RedisVectorStore.Algorithm.HSNW)
+                .initializeSchema(true)
+                .build();
     }
-
 }
